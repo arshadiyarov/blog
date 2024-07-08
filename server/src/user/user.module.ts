c630e58e22ff178685d8ba4c45f9d3cef81bf1eb
arshadiyarov <shadiyarovarmat@gmail.com>
2024-07-08 18:18:56 +0500

    video-03: added auth module, added login endpoint and jwt and password hashing methods

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './service/user.service';
-import { UserController } from './controller/user.controller';
+import { UserService } from './services/user.service';
+import { UserController } from './controllers/user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './models/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
   providers: [UserService],
   controllers: [UserController],
 })
